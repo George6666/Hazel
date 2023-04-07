4f1baa3dbae7b445f4186663d4769137f45d2e98
pengkefan <983218361@qq.com>
2023-04-07 21:12:00 +0800

    添加项目文件。

---
+++ Hazel/src/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+#include <Stdio.h>
+
+namespace Hazel {
+	void print() {
+		printf("Welcome to Hazel Engine!\n");
+	}
+}
---
+++ SandBox/src/Application.cpp
@@ -0,0 +1,8 @@
+namespace Hazel {
+	_declspec(dllexport) void print();
+}
+
+
+void main() {
+	Hazel::print();
+}
